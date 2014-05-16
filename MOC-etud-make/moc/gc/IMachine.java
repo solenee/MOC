84c8f25fa24f25d467e9265a21ef1ab4057563f0
solenee <kehsa@hotmail.fr>
2014-05-16 20:40:24 +0200

    version fusion type + tds

---
+++ MOC-etud-make/moc/gc/IMachine.java
@@ -0,0 +1,150 @@
+package moc.gc;
+
+import moc.compiler.MOCException;
+
+/**
+ * Cette interface décrit une machine cible. A compléter, selon votre modèle
+ * 
+ * @author marcel
+ * 
+ */
+public interface IMachine {
+	/**
+	 * Suffixe du fichier cible (.tam par exemple)
+	 * 
+	 * @return
+	 */
+	String getSuffixe();
+
+	/**
+	 * Ecrit le code dans un fichier à partir du nom du fichier source et du
+	 * suffixe
+	 * 
+	 * @param fileName
+	 * @param code
+	 * @throws MOCException
+	 */
+	void writeCode(String fileName, String code) throws MOCException;
+	
+	/**
+	 * Generate the code to free a specific size of space into the the stack
+	 * 
+	 * @param size size of the space to free
+	 * @throws MOCException
+	 */
+	public String genFree(int size);
+
+	/**
+	 * Generate a specified comment
+	 * 
+	 * @param c the comment
+	 * @throws MOCException
+	 */
+	public String genComment(String c);
+	
+	/**
+	 * Insert ASM code in line
+	 * 
+	 * @param c code to insert
+	 * @throws MOCException
+	 */
+	public String genCodeInLine(String c);
+	
+	/**
+	 * Generate a label
+	 * @throws MOCException
+	 */
+	public String genLabel();
+	
+	/**
+	 * Generate a "if then ..."
+	 * 
+	 * @param codeCondition
+	 * @param codeThen
+	 * @param codeElse vaut "" s'il n'y a pas de else
+	 * @throws MOCException
+	 */
+	public String genIf(String codeCondition, String codeThen, String codeElse);
+	
+	/**
+	 * Generate a variable declaration without initialization
+	 * 
+	 * @param ident identifier of the declared variable
+	 * @param taille size of the variable
+	 * @throws MOCException
+	 */
+	public String genDeclaration(String ident, int taille);
+	
+	/**
+	 * Generate a variable declaration with an initialization
+	 * 
+	 * @param ident identifier of the declared variable
+	 * @param taille size of the variable
+	 * @param initCode initialization code
+	 * @throws MOCException
+	 */
+	public String genDefinition(String ident, int taille, String initCode);
+	
+	/**
+	 * Generate a unary operation
+	 * 
+	 * @param codeOp code of the unary operator
+	 * @param codeFacteur code generating the value of the facteur
+	 * @throws MOCException
+	 */
+	public String genOpUnaire(String codeOp, String codeFacteur);
+	
+	/**
+	 * Generate an integer unary operation : -
+	 * @param codeFacteur code generating the value of the facteur
+	 * @throws MOCException
+	 */
+	public String genIMoins(String codeFacteur);
+	
+	/**
+	 * Generate a boolean unary operation : not 
+	 * @param codeFacteur code generating the value of the facteur
+	 * @throws MOCException
+	 */
+	public String genBNegation(String codeFacteur);
+	
+	/**
+	 * Generate an integer constant
+	 * @param c 
+	 * @throws MOCException
+	 */
+	public String genEntier(String c);
+	
+	/**
+	 * Generate a character  
+	 * @param c
+	 * @throws MOCException
+	 */
+	public String genCaractere(String c);
+
+	/**
+	 * Generate the code for calling a fonction  
+	 * @param etiquette label of the fonction
+	 * @throws MOCException
+	 */
+	public String genCall(String etiquette);
+	
+	/**
+	 * Generate the code for an affectation  
+	 * @param etiquette label of the fonction
+	 * @throws MOCException
+	 */
+	public String genAffectation(String etiquette);
+	
+	/**
+	 * Associate the code of a function to a label  
+	 * @param ident name of the fonction 
+	 * @param tailleparams 
+	 * @param tailleretour
+	 * @param code code of the fonction
+	 * @throws MOCException
+	 */
+	public String genFonction(String etiquette, int tailleparams,
+			int tailleretour, String code);
+	
+}
---
+++ MOC-etud-make/moc/gc/MTAM.java
@@ -0,0 +1,120 @@
+package moc.gc;
+
+
+/**
+ * La machine TAM et ses fonctions de generation
+ * @author marcel
+ *
+ */
+public class MTAM extends AbstractMachine {
+
+	@Override
+	public String getSuffixe() {
+		return "tam";
+	}
+	
+	@Override
+	public String genFree(int size) {
+		return "\tPOP(0)" + size + "\n";
+	}
+	
+	@Override
+	public String genComment(String c) {
+		return "; " + c + "\n";
+	}
+
+	@Override
+	public String genCodeInLine(String c) {
+		return genComment("code in inline : start\n") + c + genComment("code in inline : end\n");
+	}
+	
+	// counter to generate unique labels
+	private static int cpt = 0;
+
+	/** Generate a unique label
+	 * 
+	 * @return the label
+	 */
+	public String genEtiq() {
+		return "label" + cpt++;
+	}
+	
+	@Override
+	public String genLabel() {
+		throw new RuntimeException("Undefined method");
+	}
+
+	@Override
+	public String genIf(String codeCondition, String codeThen, String codeElse) {
+		String code;
+		String labelEndIf = genLabel();
+		// check the condition
+		code = genComment("\t; if") + codeCondition + "\n" + "\tJUMPIF(0) ";
+		if (codeElse == "") {
+			code += labelEndIf + "\n"
+					+ genComment("\t; then") + codeThen + "\n";
+		} else {
+			String labelElse = genLabel();
+			code += labelElse + "\n"
+					+ genComment("\t; then") +   codeThen + "\n" + "\tJUMP " + labelEndIf + "\n" 
+					+ genComment("\t; else") + labelElse + "\n" + codeElse + "\n";
+		}
+		code += labelEndIf + "\n" + "\t; end if\n";
+		return code;
+	}
+
+	@Override
+	public String genDeclaration(String ident, int taille) {
+		return genComment("declaration sans initialisation de "+ident+ "de taille "+taille)
+				+ "PUSH "+taille; 
+	}
+
+	@Override
+	public String genDefinition(String ident, int taille, String initCode) {
+		return genComment("declaration avec initialisation de "+ident+ "de taille "+taille)
+				+ initCode;
+	}
+
+	@Override
+	public String genOpUnaire(String codeOp, String codeFacteur) {
+		return codeFacteur +
+				"SUBR "+codeOp+"\n";
+	}
+
+	@Override
+	public String genIMoins(String codeFacteur) {
+		return genOpUnaire("INeg", codeFacteur);
+	}
+
+	@Override
+	public String genBNegation(String codeFacteur) {
+		return genOpUnaire("BNeg", codeFacteur);
+	}
+
+	@Override
+	public String genEntier(String c) {
+		throw new RuntimeException("Undefined method");
+	}
+
+	@Override
+	public String genCaractere(String c) {
+		throw new RuntimeException("Undefined method");
+	}
+
+	@Override
+	public String genCall(String etiquette) {
+		return genComment("Appel de" + etiquette) + "CALL[SB] " + etiquette + "\n";
+	}
+
+	@Override
+	public String genAffectation(String etiquette) {
+		throw new RuntimeException("Undefined method");
+	}
+
+	@Override
+	public String genFonction(String etiquette, int taillepars, int tailleretour, String code) {
+		return "_" + etiquette + ":\n" 
+				+ code + "\tRETURN (" + taillepars + ") "+ tailleretour + "\n";
+	}
+
+}
