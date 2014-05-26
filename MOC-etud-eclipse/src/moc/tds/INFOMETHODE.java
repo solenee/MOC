645e67daaf73434d1b5edb6b9b3ac9c96dec0450
Selove <selove.okecodjo@gmail.com>
2014-05-26 22:51:34 +0200

    ajout de l'attribut etiquette pour les info methodes

--- MOC-etud-eclipse/src/moc/tds/INFOMETHODE.java
+++ MOC-etud-eclipse/src/moc/tds/INFOMETHODE.java
@@ -6,17 +6,32 @@ public class INFOMETHODE extends INFOFONCTION {
 
 	protected boolean classmethod; // true si de classe, false si d'instance
 	protected Integer numero;
+	protected String etiquette;
 	
 	public INFOMETHODE(DTYPE r, boolean classmethod, Integer numero) {
 		super(r);
 		this.classmethod = classmethod;
 		this.numero = numero;
 	}
+	
+	public INFOMETHODE(DTYPE r, boolean classmethod, Integer numero, String etiquette) {
+		super(r);
+		this.classmethod = classmethod;
+		this.numero = numero;
+		this.etiquette = etiquette;
+	}
 
 	public INFOMETHODE(DTYPE r, PARAMETRES params, boolean classmethod, Integer numero) {
 		super(r, params);
 		this.classmethod = classmethod;
 		this.numero = numero;
+	}
+	
+	public INFOMETHODE(DTYPE r, PARAMETRES params, boolean classmethod, Integer numero, String etiquette) {
+		super(r, params);
+		this.classmethod = classmethod;
+		this.numero = numero;
+		this.etiquette = etiquette;
 	}
 
 	public boolean isClassmethod() {
@@ -33,6 +48,14 @@ public class INFOMETHODE extends INFOFONCTION {
 
 	public void setNumero(Integer numero) {
 		this.numero = numero;
+	}
+
+	public String getEtiquette() {
+		return etiquette;
+	}
+
+	public void setEtiquette(String etiquette) {
+		this.etiquette = etiquette;
 	}
 
 }
